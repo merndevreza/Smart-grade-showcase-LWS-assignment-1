8e925fdbb305aab31e2c086487b8c6a0efa7ce03
merndevreza <merndevreza@gmail.com>
2024-01-18 04:14:41 +0600

    Added Navbar and Hero Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
+import Hero from "./Components/Hero";
+import Navbar from "./Components/Navbar";
 
 
 export default function App() {
   return (
-    <>
+    <> 
+    <Navbar/>
+    <Hero/>
     </>
   );
 }
